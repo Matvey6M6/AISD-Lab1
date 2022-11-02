74b120025abee349f6ffda5a6360c27054e6b36b
Matvey6M6 <115408332+Matvey6M6@users.noreply.github.com>
2022-11-02 06:16:00 +0400

    Hotfix
    - Hotfix operators + and -

--- MnogochlenExceptions.hpp
+++ MnogochlenExceptions.hpp
@@ -1,28 +1,53 @@
 #pragma once
+#include <conio.h>
+#include <math.h> /* for sqrt(), fabs(), pow(), cos(), acos(). */
+#define M_PI (3.141592653589793)
+#define M_2PI (2. * M_PI)
 #include <iostream>
-
+#include "MnogochlenExceptions.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
 using namespace std;
 
-class MnogochlenExceptions : public exception
+struct Node
 {
-protected:
-    const char *Message;
-
-    MnogochlenExceptions();
-
-    void SetMessage(const char *Message);
-
-    void PrintInfo() const;
+    long long MyOrder;
+    double Value;
+    Node *Next;
 };
 
-class MemoryError : MnogochlenExceptions
+class Mnogochlen
 {
-public:
-    MemoryError(const char *Message);
-};
+    Node *Head;
 
-class RangeError : MnogochlenExceptions
-{
+    long long OrderOfMnogochlen;
+
+    Mnogochlen Normalize() const;
+
 public:
-    RangeError(const char *Message);
+    Node *GetHead() const;
+
+    long long GetOrderOfMnogochlen() const;
+
+    Mnogochlen(long long Order);
+
+    ~Mnogochlen();
+
+    void Set(long long Order, double Coef);
+
+    double operator[](long long Order) const;
+
+    Mnogochlen operator+(const Mnogochlen &other) const;
+
+    Mnogochlen operator-(const Mnogochlen &other) const;
+
+    Mnogochlen operator*(double Val) const;
+
+    double CountValue(double x) const;
+
+    //void GetRoots() const;
+
+    friend ostream &operator<<(ostream &os, const Mnogochlen &Obj);
 };
